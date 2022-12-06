1277341ba63b08311101092efa063eb2940aff01
add-bakkers <add.re.0828@gmail.com>
2022-12-07 03:13:51 +0900

    add:verify.go

--- src/main.go
+++ src/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -13,8 +10,6 @@ import (
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
 )
 
 func main() {
@@ -52,7 +47,6 @@ func main() {
 	mux.HandleFunc("/articles/", withCORS(s.HandleArticles))
 	mux.HandleFunc("/members/", withCORS(NeedToken(s.HandleMembers)))
 	mux.HandleFunc("/members_list/", withCORS(s.HandleMembersList))
-	mux.HandleFunc("/settoken/", withCORS(s.HandleSetToken))
 	mux.HandleFunc("/github_team/", withCORS(s.HandleGithubTeam))
 	http.ListenAndServe(":8888", mux)
 }
@@ -74,76 +68,4 @@ func withCORS(fn http.HandlerFunc) http.HandlerFunc {
 		}
 		fn(w, r)
 	}
-}
-
-type Token struct {
-	Token string `json:"token"`
-}
-
-type GithubUser struct {
-	Login string `json:"login"`
-	ID    int    `json:"id"`
-}
-
-var (
-	oauthConfig = &oauth2.Config{
-		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
-		ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
-		RedirectURL:  "https://blog.winc.ne.jp/oauth/callback",
-		Scopes:       []string{"user"},
-		Endpoint:     github.Endpoint,
-	}
-)
-
-func NeedToken(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var OAuthToken Token
-		decodeBody(r, &OAuthToken)
-		token := &oauth2.Token{AccessToken: OAuthToken.Token}
-		client := oauthConfig.Client(context.Background(), token)
-		resp, err := client.Get("https://api.github.com/user")
-		if err != nil {
-			respondErr(w, r, http.StatusBadRequest, fmt.Sprintf("Failed to retrieve user info: %s", err.Error()))
-			return
-		}
-		defer resp.Body.Close()
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			respondErr(w, r, http.StatusInternalServerError)
-			return
-		}
-		var user GithubUser
-		err = json.Unmarshal(body, &user)
-		if err != nil {
-			respondErr(w, r, http.StatusInternalServerError)
-			return
-		}
-		fn(w, r)
-	}
-}
-
-func (s *Server) GetCurrentUser(w http.ResponseWriter, r *http.Request) (string, error) {
-	var OAuthToken Token
-	decodeBody(r, &OAuthToken)
-	token := &oauth2.Token{AccessToken: OAuthToken.Token}
-	client := oauthConfig.Client(context.Background(), token)
-	resp, err := client.Get("https://api.github.com/user")
-	if err != nil {
-		respondErr(w, r, http.StatusBadRequest, fmt.Sprintf("Failed to retrieve user info: %s", err.Error()))
-		return "", err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		respondErr(w, r, http.StatusInternalServerError)
-		return "", err
-	}
-	var user GithubUser
-	err = json.Unmarshal(body, &user)
-	log.Println("user:", user)
-	if err != nil {
-		respondErr(w, r, http.StatusInternalServerError)
-		return "", err
-	}
-	return user.Login, nil
 }
---
+++ src/verify.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+)
+
+type Token struct {
+	Token string `json:"token"`
+}
+
+type GithubUser struct {
+	Login string `json:"login"`
+	ID    int    `json:"id"`
+}
+
+var (
+	oauthConfig = &oauth2.Config{
+		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
+		ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
+		RedirectURL:  "https://blog.winc.ne.jp/oauth/callback",
+		Scopes:       []string{"user"},
+		Endpoint:     github.Endpoint,
+	}
+)
+
+func NeedToken(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var OAuthToken Token
+		decodeBody(r, &OAuthToken)
+		token := &oauth2.Token{AccessToken: OAuthToken.Token}
+		client := oauthConfig.Client(context.Background(), token)
+		resp, err := client.Get("https://api.github.com/user")
+		if err != nil {
+			respondErr(w, r, http.StatusBadRequest, fmt.Sprintf("Failed to retrieve user info: %s", err.Error()))
+			return
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			respondErr(w, r, http.StatusInternalServerError)
+			return
+		}
+		var user GithubUser
+		err = json.Unmarshal(body, &user)
+		if err != nil {
+			respondErr(w, r, http.StatusInternalServerError)
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func (s *Server) GetCurrentUser(w http.ResponseWriter, r *http.Request) (string, error) {
+	var OAuthToken Token
+	decodeBody(r, &OAuthToken)
+	token := &oauth2.Token{AccessToken: OAuthToken.Token}
+	client := oauthConfig.Client(context.Background(), token)
+	resp, err := client.Get("https://api.github.com/user")
+	if err != nil {
+		respondErr(w, r, http.StatusBadRequest, fmt.Sprintf("Failed to retrieve user info: %s", err.Error()))
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		respondErr(w, r, http.StatusInternalServerError)
+		return "", err
+	}
+	var user GithubUser
+	err = json.Unmarshal(body, &user)
+	log.Println("user:", user)
+	if err != nil {
+		respondErr(w, r, http.StatusInternalServerError)
+		return "", err
+	}
+	return user.Login, nil
+}
