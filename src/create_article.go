1ad1f2ccd952fb814a7db2ff47f3ae8eb7f20bd3
add-bakkers <add.re.0828@gmail.com>
2022-12-07 23:27:26 +0900

    update:create_article

--- src/create_article.go
+++ src/create_article.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"net/http"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Article struct {
 	UUID      string
-	ID        string `json:"id"`
+	GithubID  string `json:"githubid"`
 	Title     string `json:"title"`
+	Text      string `json:"text"`
 	Published time.Time
 }
 
@@ -34,12 +37,17 @@ func (s *Server) HandleCreateArticlePost(w http.ResponseWriter, r *http.Request)
 		respondErr(w, r, http.StatusBadRequest, "", err)
 		return
 	}
-	if githubid != article.ID {
-		respondErr(w, r, http.StatusBadRequest, "", err)
-		return
-	}
 	db := s.client.Database("winc")
 	collection := db.Collection("articles_original")
+	article.Published = time.Now()
+	article.Published.Format("2006-01-02")
+	article.GithubID = githubid
+	u, err := uuid.NewRandom()
+	if err != nil {
+		respondErr(w, r, http.StatusInternalServerError, err)
+		return
+	}
+	article.UUID = u.String()
 	_, err = collection.InsertOne(context.TODO(), article)
 	if err != nil {
 		respondErr(w, r, http.StatusInternalServerError, err)
